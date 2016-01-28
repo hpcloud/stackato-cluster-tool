8d8d3b7b37c90ff86a9be2fcdd24a0f809ef6e86
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-01-28 11:46:57 -0800

    Add a Cloudinit userdata for Amazon

---
+++ amazon-aws/template-file-cloudinit-userdata.tf
@@ -0,0 +1,38 @@
+# Cloudinit user data to set up a Core node
+resource "template_file" "core" {
+  template = "${file("template-file-cloudinit-core.tpl")}"
+
+  vars {
+      cluster_hostname = "${var.cluster_hostname}"
+      core_password = "${var.core_password}"
+      stackato_automation_path = "${var.stackato_automation_path}"
+  }
+}
+
+# Cloudinit user data to set up DEA nodes
+resource "template_file" "dea" {
+  depends_on = [ "aws_instance.core"]
+  template = "${file("template-file-cloudinit-nodes.tpl")}"
+
+  vars {
+      core_ip = "${aws_instance.core.private_ip}"
+      core_password = "${var.core_password}"
+      roles = "${lookup(var.dea, "roles")}"
+      wait_core_timeout = "${var.wait_core_timeout}"
+      cluster_hostname = "${var.cluster_hostname}"
+  }
+}
+
+# Cloudinit user data to set up Dataservices nodes
+resource "template_file" "dataservices" {
+  depends_on = [ "aws_instance.core"]
+  template = "${file("template-file-cloudinit-nodes.tpl")}"
+
+  vars {
+      core_ip = "${aws_instance.core.private_ip}"
+      core_password = "${var.core_password}"
+      roles = "${lookup(var.dataservices, "roles")}"
+      wait_core_timeout = "${var.wait_core_timeout}"
+      cluster_hostname = "${var.cluster_hostname}"
+  }
+}

eb1037b6b88084a7c78308eebe0c0a3d08cb6cb5
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-04 15:40:29 -0800

    Add functions to check the node is ready

---
+++ stackato-automation/lib/node_ready.sh
@@ -0,0 +1,26 @@
+export FLAG_DIR=${FLAG_DIR:-"/home/stackato"}
+
+function node_ready.set_flag() {
+  local flag_name="${1:?missing input}"
+
+  kato_op_defer_commands "touch ${FLAG_DIR}/$flag_name"
+}
+
+function node_ready.wait_flag() {
+  local flag_name="${1:?missing input}"
+  local sleep_time="${2:-5}"
+
+  local flag_path="${FLAG_DIR}/$flag_name"
+
+  while [ ! -f "$flag_path" ]; do
+    >&2 echo "Waiting for node to be ready (flag $flag_path)"
+    sleep $sleep_time
+  done
+
+}
+
+function node_ready.delete_flag() {
+  local flag_name="${1:?missing input}"
+
+  rm $FLAG_DIR/$flag_name
+}

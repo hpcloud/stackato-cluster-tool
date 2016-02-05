765cd3d7b2d01f0f436a9da57846b4c9e5894f68
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-05 10:27:02 -0800

    [lib/ssh.sh] Add more functions - Add ssh_get_remote_public_key - Add ssh_copy_ssh_key

--- stackato-automation/lib/ssh.sh
+++ stackato-automation/lib/ssh.sh
@@ -1,5 +1,5 @@
 function ssh_set_PasswordAuthentication {
-  local enabled="$1"
+  local enabled="${1:?missing input}"
 
   local sshd_config="/etc/ssh/sshd_config"
   local sshd_property="PasswordAuthentication"
@@ -12,9 +12,9 @@ function ssh_set_PasswordAuthentication {
 }
 
 function ssh_set_StrictHostKeyChecking {
-  local ssh_config="$1"
-  local host_filter="$2"
-  local enabled="$3"
+  local ssh_config="${1:?missing input}"
+  local host_filter="${2:?missing input}"
+  local enabled="${3:?missing input}"
 
   local sshd_property="StrictHostKeyChecking"
 
@@ -23,4 +23,22 @@ function ssh_set_StrictHostKeyChecking {
   else
     >&2 echo "ssh_set_passwordauthentication - unknown state $enabled"
   fi
+}
+
+function ssh_get_remote_public_key() {
+  local ip="${1:?missing input}"
+  local user="${2:?missing input}"
+  local password="${3:?missing input}"
+
+  local public_key="/home/$user/.ssh/id_rsa.pub"
+
+  sshpass -p "${password}" ssh -o StrictHostKeyChecking=no ${user}@${ip} cat $public_key
+}
+
+function ssh_copy_ssh_key() {
+  local ip="${1:?missing input}"
+  local user="${2:?missing input}"
+  local password="${3:?missing input}"
+
+  run_as "stackato" "sshpass -p \"${password}\" ssh-copy-id ${user}@${ip}"
 }

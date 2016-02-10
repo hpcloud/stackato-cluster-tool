46c5774885bf02cb7b2341d9e6a6267e1afe117c
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-10 11:11:53 -0800

    [Stackato-automation] Simplify controller_move_data
    and add input ssh_pubkey_path

--- stackato-automation/roles/cloud-controller.sh
+++ stackato-automation/roles/cloud-controller.sh
@@ -20,18 +20,19 @@ function controller_configure() {
   local stackato_user="${STACKATO_USER:=stackato}"
   local stackato_group="${STACKATO_GROUP:=stackato}"
   local fuse_conf_path="${FUSE_CONF_PATH:=/etc/fuse.conf}"
+  local ssh_pubkey_path="${SSH_PUBKEY_PATH:-/home/stackato/.ssh/id_rsa.pub}"
 
   kato_role_add "controller"
   controller_make_new_environment ${controller_mount_dir} ${stackato_user} ${stackato_group}
 
   # Connect to the controller shared directory
   if ! ip -4 address | grep -q ${controller_shared_dir_ip}; then
-    ssh_copy_ssh_key ${controller_shared_dir_ip} ${stackato_user} ${controller_shared_dir_password}
+    ssh_copy_ssh_key ${controller_shared_dir_ip} ${stackato_user} ${controller_shared_dir_password} ${ssh_pubkey_path}
     sshfs_wait_source ${controller_shared_dir_ip} ${stackato_user} ${controller_shared_dir} 5
     sshfs_mount ${controller_shared_dir_ip} ${stackato_user} ${controller_shared_dir} ${controller_mount_dir}
   fi
 
-  controller_move_data ${data_dir} ${stackato_user} ${controller_mount_dir}
+  controller_move_data ${data_dir} ${data_cc_droplets_dir} ${controller_mount_dir}
   kato_role "controller" "start"
 }
 
@@ -42,9 +43,9 @@ function controller_make_new_environment() {
 
   # Perform the following actions on the core node and each additional controller node
   ## Create a mount point
-  sudo mkdir -p $controller_mount_dir
+  mkdir -p $controller_mount_dir
   ## Give stackato ownership of the mount point
-  sudo chown -R $dir_user:$dir_group $controller_mount_dir
+  chown -R $dir_user:$dir_group $controller_mount_dir
 }
 
 function controller_move_data() {
@@ -52,16 +53,15 @@ function controller_move_data() {
   local data_cc_droplets_dir="${2:?missing input}"
   local controller_mount_dir="${3:?missing input}"
 
-  ## Move aside the original data directory
-  [ -d "$data_dir" ] && sudo mv $data_dir $data_dir.old
-  ## Create a symlink from the data directory to the mount point
-  [ ! -L "$data_dir" ] && sudo ln -s $controller_mount_dir $data_dir
-  ## Move the original staged_droplet_uploads directory
-  [ -d "$data_cc_droplets_dir" ] && sudo mv $data_cc_droplets_dir $data_cc_droplets_dir.old
-  ## Create a symlink from staged_droplet_uploads directory to the mount point
-  [ ! -L "$data_cc_droplets_dir" ] && sudo ln -s $controller_mount_dir $data_cc_droplets_dir
-
-  # Copy the controller data and the droplets on the core node into the shared directory
-  sudo rsync -a $data_dir.old/ $data_dir
-  sudo rsync -a $data_cc_droplets_dir.old/ $data_cc_droplets_dir
+  for dir in $data_dir $data_cc_droplets_dir; do
+    if [ -d "$dir" ]; then
+          # Move aside the original directory
+          mv $dir $dir.old
+          # Create a symlink from the directory to the mount point
+          ln -s $controller_mount_dir $dir
+          # Copy the controller data into the shared directory
+          rsync -a $dir.old/ $dir
+          rm -r $dir.old
+    fi
+  done
 }

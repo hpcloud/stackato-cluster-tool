5ba8e62c0b4b12234e1d546935d4415e68c72b46
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-10 10:59:36 -0800

    [OpenStack] Create Cloud Controller instances with Terraform

--- openstack/provider-openstack-instances.tf
+++ openstack/provider-openstack-instances.tf
@@ -71,3 +71,23 @@ resource "openstack_compute_instance_v2" "dataservices" {
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.dataservices.rendered}"
 }
+
+resource "openstack_compute_instance_v2" "controller" {
+  depends_on = [ "openstack_networking_subnet_v2.public" ]
+  region = "${var.os_region_name}"
+  # Amount of nodes
+  count = "${lookup(var.controller, "count")}"
+  # Launch the instance
+  name = "${var.cluster_name}-controller-${format("%03d", count.index + 1)}"
+  image_id = "${lookup(var.openstack_images, var.os_region_name)}"
+  flavor_name = "${lookup(var.openstack_flavor_name, "controller")}"
+  key_pair = "${var.ssh_key_name}"
+  security_groups = ["${openstack_compute_secgroup_v2.private.id}"]
+  network {
+    uuid="${openstack_networking_network_v2.main.id}"
+    name="${openstack_networking_network_v2.main.name}"
+  }
+  # scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.private.cidr}" }
+  scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
+  user_data = "${template_file.controller.rendered}"
+}

2cf3c20c05a924dd85625ce3f7682ace2e0f3a16
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-10 11:05:50 -0800

    [Stackato-automation] Add func to defer attach node and scripts
    and delete (moved to kato.sh as simple kato functions):
    - kato_defer_node_rename
    - kato_defer_node_setup_core

--- stackato-automation/lib/kato-defer.sh
+++ stackato-automation/lib/kato-defer.sh
@@ -3,12 +3,32 @@ function kato_op_defer {
   run_as "stackato" "/home/stackato/bin/kato op defer \"$cmd\" --run-as-root --post-start"
 }
 
-function kato_defer_node_rename {
-  local cluster_hostname="${1:?undefined input}"
-  kato_op_defer "node rename $cluster_hostname --no-restart"
+function kato_defer_attach_node() {
+  local core_ip="${1:?undefined input}"
+  local core_user="${2:?undefined input}"
+  local core_password="${3:?undefined input}"
+
+  local core_ssh_pub_key="/home/$core_user/.ssh/id_rsa.pub"
+  local kato_bin="/home/$core_user/bin/kato"
+
+  kato_op_defer "version; sshpass -p ${core_password} ssh-copy-id -i $core_ssh_pub_key ${core_user}@${core_ip}; $kato_bin node attach ${core_ip}"
 }
 
-function kato_defer_node_setup_core {
-  local cluster_hostname="${1:?undefined input}"
-  kato_op_defer "node setup core api.${cluster_hostname}"
+function kato_op_defer_script() {
+  local script="${1:?undefined input}"
+  local script_opts="${@:2}"
+
+  chmod u+x ${script}
+
+  if [ -z "${script_opts}" ]; then
+    kato_op_defer "version; ${script}"
+  else
+    kato_op_defer "version; ${script} ${script_opts[@]}"
+  fi
+}
+
+function kato_op_defer_commands() {
+  local commands="${1:?missing input}"
+
+  kato_op_defer "version; ${commands}"
 }

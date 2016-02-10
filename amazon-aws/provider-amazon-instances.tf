b0ea4a29b3f640bd5f5468e49d1777676c58f7ca
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-10 10:57:04 -0800

    [Amazon] Create Cloud Controller instances with Terraform

--- amazon-aws/provider-amazon-instances.tf
+++ amazon-aws/provider-amazon-instances.tf
@@ -73,3 +73,22 @@ resource "aws_instance" "dataservices" {
   # Provision the node
   user_data = "${template_file.dataservices.rendered}"
 }
+
+resource "aws_instance" "controller" {
+  # Launch the instance after the VPC and the Core node
+  depends_on = [ "aws_internet_gateway.gw", "aws_vpc.main", "aws_subnet.private",
+    "aws_instance.core" ]
+  # Amount of nodes
+  count = "${lookup(var.controller, "count")}"
+  # Launch the instance
+  ami = "${lookup(var.amazon_images, var.region)}"
+  instance_type = "${lookup(var.aws_instance_type, "controller")}"
+  key_name = "${var.ssh_key_name}"
+  # Give a name to the node
+  tags { Name = "${var.cluster_name}-controller-${format("%03d", count.index + 1)}" }
+  # The VPC Subnet ID to launch in
+  subnet_id = "${aws_subnet.private.id}"
+  vpc_security_group_ids = [ "${aws_security_group.stackato_backend.id}" ]
+  # Provision the node
+  user_data = "${template_file.controller.rendered}"
+}

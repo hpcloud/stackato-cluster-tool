e46aba6af5c45d2f8654f80e68e670ef16e5a269
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-10 11:00:37 -0800

    [OpenStack] Add Cloud Controller userdata
    + roles list for the Core node
    + stackato_automation_path for DEA and Dataservices nodes

--- openstack/template-file-cloudinit-userdata.tf
+++ openstack/template-file-cloudinit-userdata.tf
@@ -5,6 +5,7 @@ resource "template_file" "core" {
   vars {
       cluster_hostname = "${var.cluster_hostname}"
       core_password = "${var.core_password}"
+      roles = "${lookup(var.core, "roles")}"
       stackato_automation_path = "${var.stackato_automation_path}"
   }
 }
@@ -19,6 +20,7 @@ resource "template_file" "dea" {
       roles = "${lookup(var.dea, "roles")}"
       wait_core_timeout = "${var.wait_core_timeout}"
       cluster_hostname = "${var.cluster_hostname}"
+      stackato_automation_path = "${var.stackato_automation_path}"
   }
 }
 
@@ -32,5 +34,20 @@ resource "template_file" "dataservices" {
       roles = "${lookup(var.dataservices, "roles")}"
       wait_core_timeout = "${var.wait_core_timeout}"
       cluster_hostname = "${var.cluster_hostname}"
+      stackato_automation_path = "${var.stackato_automation_path}"
+  }
+}
+
+# Cloudinit user data to set up Dataservices nodes
+resource "template_file" "controller" {
+  template = "${file("template-file-cloudinit-nodes.tpl")}"
+
+  vars {
+      core_ip = "${openstack_compute_instance_v2.core.network.0.fixed_ip_v4}"
+      core_password = "${var.core_password}"
+      roles = "${lookup(var.controller, "roles")}"
+      wait_core_timeout = "${var.wait_core_timeout}"
+      cluster_hostname = "${var.cluster_hostname}"
+      stackato_automation_path = "${var.stackato_automation_path}"
   }
 }

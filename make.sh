06c15d310d9b7b3b2f7e4f3eb82fcb2f1dd1bfe5
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-15 14:29:11 -0800

    make.sh - Add option --out

--- make.sh
+++ make.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -e # Exit on error
 
-# Inputs
-platform="$1"
-
-# Configuration
-PLATFORM_DEFAULT="openstack"
-VERSION_DEFAULT="stackato-version/var-stackato-361.tf"
-
 # Constants
 CWD="$(readlink -f $(dirname $0))"; cd $CWD
 PLATFORM_SUPPORT=(openstack amazon-aws)          # Supported platforms by this tool
-OUTPUT_DIR_NAME="out"
-OUTPUT_DIR="$CWD/$OUTPUT_DIR_NAME"
+
+# Default configuration
+PLATFORM_DEFAULT="openstack"
+VERSION_DEFAULT="terraform/stackato-version/var-stackato-361.tf"
+OUTPUT_DIR_DEFAULT="$CWD/out"
 
 ## Message
 MSG_UNSUPPORTED_PLATFORM="The platform %s is not supported\n"
@@ -26,13 +22,16 @@ function usage(){
   >&2 echo "
   usage: $0 [OPTIONS] [-p | --platform PLATFORM] [-v | --version VERSION]
 
-  Create a folder '$OUTPUT_DIR_NAME' with the Terraform files to deploy a cluster
+  Create a folder with the Terraform files to deploy a cluster
   on the plaform PLATFORM.
 
   PLATFORM = ${PLATFORM_SUPPORT[@]} (default: $PLATFORM_DEFAULT)
   VERSION = Stackato version file path (default: $VERSION_DEFAULT)
 
   OPTIONS:
+    -o | --out: Output directory (default: $OUTPUT_DIR_DEFAULT)
+    -p | --platform: Platform to deploy (default: $PLATFORM_DEFAULT)
+    -v | --version: Path of the Stackato version file (default: $VERSION_DEFAULT)
     -h | --help: Show help message
     -d | --debug: Turn on the debug mode
 
@@ -69,9 +68,12 @@ function copy_terraform_config() {
   local platform="$1"
   local version="$2"
   local output_dir="$3"
-  local platform_dir="$CWD/$platform"
-  local version_dir="$CWD/$version"
+
+  local platform_dir="$CWD/terraform/$platform"
+  local version_dir="$version"
+  local common_tf_dir="$CWD/terraform/common"
   local provisioner_dir="$CWD/stackato-automation"
+  local cloudinit_dir="$CWD/cloudinit/*"
 
   if [ ! -d "$platform_dir" ]; then
     message "error" "$MSG_PLATFORM_DIR_NOT_FOUND" "$platform_dir"
@@ -81,7 +83,9 @@ function copy_terraform_config() {
     find $CWD -maxdepth 1 -type f \( -name "*.tf" -or -name "*.tpl" \) -exec cp {} $output_dir \;
     cp $platform_dir/* $output_dir
     cp $version_dir $output_dir/var-stackato-version.tf
+    cp $common_tf_dir/* $output_dir
     cp -r $provisioner_dir $output_dir
+    cp -r $cloudinit_dir $output_dir
     message "info" "$MSG_TERRAFORM_CONFIG_COPY_DONE" "$version" "$platform"
     message "stdout" "$output_dir\n"
   fi
@@ -90,16 +94,18 @@ function copy_terraform_config() {
 
 # Main function
 function main() {
-  local platform="$PLATFORM_DEFAULT" # Platform hosting the cluster
-  local version="$VERSION_DEFAULT"   # Version to deploy
+  local platform="$PLATFORM_DEFAULT"     # Platform hosting the cluster
+  local version="$VERSION_DEFAULT"       # Version to deploy
+  local output_dir="$OUTPUT_DIR_DEFAULT" # Default output dir
 
   # Parse parameters
   while true; do
     case "$1" in
-      -d | --debug ) set -x; shift ;;
-      -h | --help  ) usage ; exit 1 ;;
-      -p | --platform ) shift; platform="$1"; shift;;
-      -v | --version ) shift; version="$1"; shift;;
+      -d | --debug )    set -x; shift ;;
+      -h | --help  )    usage ; exit 1 ;;
+      -p | --platform ) shift; platform="$1";   shift;;
+      -v | --version )  shift; version="$1";    shift;;
+      -o | --out )      shift; output_dir="$1"; shift;;
       -- ) shift; break ;;
       "" ) break ;;
       * ) echo "Invalid parameter '$1'"; usage ; exit 1 ;;
@@ -107,7 +113,7 @@ function main() {
   done
 
   check_platform_support "$platform"
-  copy_terraform_config "$platform" "$version" "$OUTPUT_DIR"
+  copy_terraform_config "$platform" "$version" "$output_dir"
 }
 
 main "$@"

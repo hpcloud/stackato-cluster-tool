4da209ff269b8c6947ff2a9d4099507cc854e077
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-02-15 18:34:03 -0800

    [Terraform] Add MPC Stackato 361 image id

--- terraform/stackato-version/var-stackato-361.tf
+++ terraform/stackato-version/var-stackato-361.tf
@@ -16,6 +16,7 @@ variable "amazon_images" {
 variable "openstack_images" {
   description = "OpenStack images of Stackato 3.6.1"
   default = {
+    region1        = "1b6e4d51-09c6-4c07-91a4-9126d6331de2" # MPC
     region-a.geo-1 = "acd5fc47-d44e-4d25-a4dd-5081dad6364d" # US West
     region-b.geo-1 = "5fa2f882-5d64-46ef-9dcd-91e33f3e7895" # US East
   }

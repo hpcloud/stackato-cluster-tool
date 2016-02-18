3be4b37e2c981e6e0c96d39729663ab390af2b8c
Hart Simha <hart.simha@hpe.com>
2016-02-17 17:25:24 -0800

    Destination should be /dev/null when not verbose

--- terraform/vbox/auto-launch.sh
+++ terraform/vbox/auto-launch.sh
@@ -10,16 +10,18 @@ set -e
 
 if [ -z "$VERBOSE_IMPORT" ]; then
   VBoxManage import $BASE_PATH/$IMAGE_NAME | tee /tmp/vboximport.log # --vmname <name>
+  DEST=/dev/stdout
 else 
   VBoxManage import $BASE_PATH/$IMAGE_NAME > /tmp/vboximport.log
+  DEST=/dev/null
 fi
 
 VM_NAME="$(grep "Suggested VM name" /tmp/vboximport.log | sed s/^[^\"]*\"// | sed s/\".*//)"
-VBoxManage modifyvm $VM_NAME --nic1 bridged --bridgeadapter1 "$INTERFACE" > /dev/null
-VBoxManage modifyvm $VM_NAME --macaddress1 auto > /dev/null
+VBoxManage modifyvm $VM_NAME --nic1 bridged --bridgeadapter1 "$INTERFACE" > $DEST 
+VBoxManage modifyvm $VM_NAME --macaddress1 auto > $DEST
 MAC_ADDR=$(VBoxManage showvminfo $VM_NAME | grep -oE "MAC: [0-9A-Z]{12}" | grep -oE "[0-9A-Z]{12}")
 MAC_ADDR=${MAC_ADDR:0:2}:${MAC_ADDR:2:2}:${MAC_ADDR:4:2}:${MAC_ADDR:6:2}:${MAC_ADDR:8:2}:${MAC_ADDR:10:2}
-VBoxManage startvm $VM_NAME > /dev/null
+VBoxManage startvm $VM_NAME > $DEST
 sleep 30
 IP_ADDR=$(arp -a | grep -i $MAC_ADDR | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
 echo $IP_ADDR

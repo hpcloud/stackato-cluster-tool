455f51ecb86ddbe521f808dc07c962773dfaff0a
Manpreet Singh <manpreet.singh@hpe.com>
2016-02-23 17:27:53 -0800

    added 1dea-1cc-1ds and var-stackato-362-rc02.tf

---
+++ terraform/stackato-version/var-stackato-362-rc02.tf
@@ -0,0 +1,22 @@
+variable "amazon_images" {
+  description = "Amazon AMIs of Stackato 3.6.2 RC01"
+  default = {
+    us-east-1      = "" # Northern Virginia
+    us-west-2      = "ami-75779415" # Oregon
+    us-west-1      = "" # Northern California
+    eu-west-1      = "" # Ireland
+    eu-central-1   = "" # Frankfurt
+    ap-southeast-1 = "" # Singapore
+    ap-southeast-2 = "" # Sydney
+    ap-northeast-1 = "" # Tokyo
+    sa-east-1      = "" # Sao Paulo
+  }
+}
+
+variable "openstack_images" {
+  description = "OpenStack images of Stackato 3.6.2 RC01"
+  default = {
+    region1        = "e5e45cd2-d4df-460e-a2ea-37bddecf8976" # MPC
+  }
+}
+

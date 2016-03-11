a6710de5ce16ad6d5e0b56d863e21be4fc7f91c2
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-03-11 15:31:48 -0800

    [provision] Core - Add debug mode and default vars

--- stackato-automation/configure-core.sh
+++ stackato-automation/configure-core.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 set -e
-set -x
+[ ! -z "$DEBUG" ] && set -x
 # Inputs
 core_password="${1:?missing input}"
 cluster_hostname="${2:?missing input}"
-stackato_automation_path="${3:?missing input}"
-roles="${4:?missing input}"
+roles="${3:?missing input}"
+
+core_user="stackato"
+cc_shared_dir="/mnt/controller"
 
 # Get and move to the current working directory
 CWD="$(dirname $0)" && cd $CWD
 # Load the libraries
 source load-libs.sh
 
+message "info" "> Start the APT Cacher"
+service_autostart "apt-cacher-ng"
+service_mgnt "apt-cacher-ng" "start"
+
 message "info" "> Waiting for node to be ready"
 node_ready.set_flag ".node_ready_flag"
 node_ready.wait_flag ".node_ready_flag"
 
 roles_array=($(echo $roles|tr "," " "))
-
-message "info" "> Start the APT Cacher"
-service_autostart "apt-cacher-ng"
-service_mgnt "apt-cacher-ng" "start"
 
 message "info" "> Update the password of the stackato account"
 change_system_password "stackato" "$core_password"
@@ -40,5 +42,5 @@ kato_node_setup_core $cluster_hostname
 # If controller role, configure it
 if [[ "${roles_array[@]/controller}" != "${roles_array[@]}" ]]; then
   message "info" "> Setup of the controller"
-  controller_configure "127.0.0.1" "${core_password}"
+  controller_configure "$cc_shared_dir" "127.0.0.1" "$core_user" "${core_password}"
 fi

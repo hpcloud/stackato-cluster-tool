c54894755b14b53203d3d53eba6bf5fe71a01774
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-03-21 13:08:38 -0700

    [terraform] AWS - Update backend security group
    Allow access from anyone from anywhere because the rules are already
    defined in the network ACL setup.

--- terraform/amazon-aws/provider-amazon-security-groups.tf
+++ terraform/amazon-aws/provider-amazon-security-groups.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "stackato_backend" {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["${aws_subnet.public.cidr_block}"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow all egress traffic
@@ -41,7 +41,7 @@ resource "aws_security_group" "stackato_backend" {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["${aws_subnet.public.cidr_block}"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
 }
 

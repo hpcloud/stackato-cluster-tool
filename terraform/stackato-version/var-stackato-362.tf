f6234a8a736748ed44a15f684d3e68ae9ea0ef9b
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-03-21 13:09:03 -0700

    [terraform] AWS - Add all 3.6.2 AMI

--- terraform/stackato-version/var-stackato-362.tf
+++ terraform/stackato-version/var-stackato-362.tf
@@ -1,22 +1,22 @@
 variable "amazon_images" {
-  description = "Amazon AMIs of Stackato 3.6.2 RC02"
+  description = "Amazon AMIs of Stackato 3.6.2"
   default = {
-    us-east-1      = "" # Northern Virginia
-    us-west-2      = "ami-18c82a78" # Oregon
-    us-west-1      = "" # Northern California
-    eu-west-1      = "" # Ireland
-    eu-central-1   = "" # Frankfurt
-    ap-southeast-1 = "" # Singapore
-    ap-southeast-2 = "" # Sydney
-    ap-northeast-1 = "" # Tokyo
-    sa-east-1      = "" # Sao Paulo
+    us-east-1      = "ami-cd6759a7" # Northern Virginia
+    us-west-2      = "ami-7d5fb21d" # Oregon
+    us-west-1      = "ami-ca98e8aa" # Northern California
+    eu-west-1      = "ami-3cfa444f" # Ireland
+    eu-central-1   = "ami-0637d269" # Frankfurt
+    ap-southeast-1 = "ami-ad9e56ce" # Singapore
+    ap-southeast-2 = "ami-3686a055" # Sydney
+    ap-northeast-1 = "ami-aacbc6c4" # Tokyo
+    ap-northeast-2 = "ami-445f912a" # Seoul
+    sa-east-1      = "ami-2961e345" # Sao Paulo
   }
 }
 
 variable "openstack_images" {
-  description = "OpenStack images of Stackato 3.6.2 RC02"
+  description = "OpenStack images of Stackato 3.6.2"
   default = {
     region1        = "e5e45cd2-d4df-460e-a2ea-37bddecf8976" # MPC
   }
 }
-

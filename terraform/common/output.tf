747dc1d3ee25772464254a906a170d24c11903b3
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-04-21 15:06:05 -0700

    [terraform] Unify Terraform outputs

--- terraform/common/output.tf
+++ terraform/common/output.tf
@@ -1,3 +1,11 @@
 output "web_console_address" {
     value = "http://api.${var.cluster_hostname}"
 }
+
+output "ssh_core_ip" {
+    value = "${null_resource.core.triggers.private_ip}"
+}
+
+output "ssh_core_access" {
+  value = "ssh -o ProxyCommand='ssh -W %h:%p ${lookup(var.proxy, "admin_user")}@${null_resource.proxy.triggers.public_ip}' stackato@${null_resource.core.triggers.private_ip}"
+}

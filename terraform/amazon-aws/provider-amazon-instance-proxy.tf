99e80f164ce4c8b1382a342ed8d8bec1723b9763
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-04-28 14:38:48 -0700

    [terraform] AWS - Add proxy in a spot instance

--- terraform/amazon-aws/provider-amazon-instance-proxy.tf
+++ terraform/amazon-aws/provider-amazon-instance-proxy.tf
@@ -32,3 +32,42 @@ resource "aws_instance" "proxy" {
     ]
   }
 }
+
+resource "aws_spot_instance_request" "proxy" {
+  # Launch the instance after the internet gateway is up
+  depends_on = [ "aws_internet_gateway.gw", "aws_vpc.main", "aws_subnet.public" ]
+  count = "${lookup(var.spot_instances, "proxy_count")}"
+  # Launch the instance
+  ami = "${lookup(var.amazon_ubuntu_images, var.region)}"
+  instance_type = "${lookup(var.aws_instance_type, "proxy")}"
+  key_name = "${var.ssh_key_name}"
+  # Give a name to the node
+  tags { Name = "${var.cluster_name}-spot-proxy-${format("%03d", count.index + 1)}" }
+  # The VPC Subnet ID to launch in and security group
+  subnet_id = "${aws_subnet.public.id}"
+  vpc_security_group_ids = [ "${aws_security_group.stackato_endpoints.id}" ]
+  # Provision the node
+  user_data = "${template_file.proxy.rendered}"
+
+  spot_price = "${lookup(var.spot_instances, "proxy_spot_price")}"
+  block_duration_minutes = "${lookup(var.spot_instances, "proxy_block_duration_minutes")}"
+  wait_for_fulfillment = "${lookup(var.spot_instances, "proxy_wait_for_fulfillment")}"
+
+  # Setup the provisioner connection with the Core node
+  connection {
+      user = "${lookup(var.provisioner_repo, "admin_user")}"
+      private_key = "${file(var.ssh_key_path)}"
+  }
+
+  provisioner "file" {
+    source = "stackato-automation"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chown -R ${lookup(var.provisioner_repo, "user")}:${lookup(var.provisioner_repo, "user")} /tmp/stackato-automation",
+      "sudo mv /tmp/stackato-automation ${lookup(var.provisioner_repo, "location")}"
+    ]
+  }
+}

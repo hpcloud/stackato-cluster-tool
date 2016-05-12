de6323e6fed3dc6ecc78978a85468b00ebd118dc
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-05-12 12:51:10 -0700

    [provision] Add usage function

--- stackato-automation/lib/setup-node.sh
+++ stackato-automation/lib/setup-node.sh
@@ -7,37 +7,79 @@
 #
 #   Optional parameters: check in the function
 #
+function usage() {
+  >&2 echo "
+    $0 BASE_CLUSTER_CONFIG [OPTIONS]
+
+    BASE_CLUSTER_CONFIG:
+
+    -c | --core-ip           IP address of the core node (e.g. 10.0.0.1)
+    -e | --cluster-hostname  Endpoint hostname of the cluster (e.g. mycluster.com)
+    -r | --roles             Comma-separated list of Stackato roles
+
+    OPTIONS:
+
+    -u | --core-user         Username to SSH into the Core node (default: stackato)
+    -p | --core-password     Password to SSH into the Core node (default: stackato)
+    -n | --node-user         Stackato node user (default: --core-user)
+
+    --mbus-ip                IP address of the MBUS server (default: --core-ip)
+    --mbus-port              Port of the MBUS server (default: 4222)
+
+    --start-apt-cacher       Start APT Cacher on the node (default: false)
+
+    --use-proxy              Use a proxy on the node
+    --http-proxy             HTTP/HTTPS proxy IP (required if --use-proxy)
+    --http-proxy-port        HTTP proxy port (default: 8123)
+    --https-proxy-port       HTTPS proxy port (default: --http-proxy-port)
+    --apt-proxy              APT proxy IP (default: --http-proxy)
+    --apt-http-proxy-port    APT HTTP proxy port (default: 3142)
+    --apt-https-proxy-port   APT HTTPS proxy port (default: --https-proxy-port)
+
+    --cc-shared-dir          Shared Cloud Controller (CC) directory path (default: /mnt/controller)
+    --cc-shared-dir-ip       IP to connect to the shared CC directory IP (default: --core-ip)
+    --cc-shared-dir-user     User to connect to the shared CC directory (default: --core-user)
+    --cc-shared-dir-password Password to connect to the shared CC directory (default: --core-password)
+
+    --router-acl-rules       Router Access Control List (ACL) rules (default: none)
+    --router-acl-drop-conn   Drop connection to minimize snooping or DoS (default: true)
+
+    -d | --debug             Run in debug mode
+    -h | --help              Print this message
+  "
+}
+
 function setup_node() {
   ########## Parse parameters ##########
   while [ $# -gt 0 ]; do
     case "$1" in
-      -c | --core-ip           ) shift; core_ip="$1";                  shift;; # Required
-      -u | --core-user         ) shift; core_user="$1";                shift;;
-      -p | --core-password     ) shift; core_password="$1";            shift;;
-      -n | --node-user         ) shift; node_user="$1";                shift;;
-      -h | --cluster-hostname  ) shift; cluster_hostname="$1";         shift;; # Required
-      -r | --roles             ) shift; roles="$1";                    shift;; # Required
+      -c | --core-ip           ) shift; local core_ip="$1";                shift;; # Required
+      -u | --core-user         ) shift; local core_user="$1";              shift;;
+      -p | --core-password     ) shift; local core_password="$1";          shift;;
+      -n | --node-user         ) shift; local node_user="$1";              shift;;
+      -e | --cluster-hostname  ) shift; local cluster_hostname="$1";       shift;; # Required
+      -r | --roles             ) shift; local roles="$1";                  shift;; # Required
 
-      --mbus-ip                ) shift; mbus_ip="$1";                  shift;;
-      --mbus-port              ) shift; mbus_port="$1";                shift;;
+      --mbus-ip                ) shift; local mbus_ip="$1";                shift;;
+      --mbus-port              ) shift; local mbus_port="$1";              shift;;
 
-      --start-apt-cacher      ) shift; start_apt_cacher="true";        shift;;
+      --start-apt-cacher       ) shift; local start_apt_cacher="true";     shift;;
 
-      --use-proxy              ) shift; use_proxy="true";;
-      --http-proxy             ) shift; http_proxy="$1";               shift;;
-      --http-proxy-port        ) shift; http_proxy_port="$1";          shift;;
-      --https-proxy-port       ) shift; https_proxy_port="$1";         shift;;
-      --apt-proxy              ) shift; apt_proxy="$1";                shift;;
-      --apt-http-proxy-port    ) shift; apt_http_proxy_port="$1";      shift;;
-      --apt-https-proxy-port   ) shift; apt_https_proxy_port="$1";     shift;;
+      --use-proxy              ) shift; local use_proxy="true";;
+      --http-proxy             ) shift; local http_proxy="$1";             shift;;
+      --http-proxy-port        ) shift; local http_proxy_port="$1";        shift;;
+      --https-proxy-port       ) shift; local https_proxy_port="$1";       shift;;
+      --apt-proxy              ) shift; local apt_proxy="$1";              shift;;
+      --apt-http-proxy-port    ) shift; local apt_http_proxy_port="$1";    shift;;
+      --apt-https-proxy-port   ) shift; local apt_https_proxy_port="$1";   shift;;
 
-      --cc-shared-dir          ) shift; cc_shared_dir="$1";            shift;;
-      --cc-shared-dir-ip       ) shift; cc_shared_dir_ip="$1";         shift;;
-      --cc-shared-dir-user     ) shift; cc_shared_dir_user="$1";       shift;;
-      --cc-shared-dir-password ) shift; cc_shared_dir_password="$1";   shift;;
+      --cc-shared-dir          ) shift; local cc_shared_dir="$1";          shift;;
+      --cc-shared-dir-ip       ) shift; local cc_shared_dir_ip="$1";       shift;;
+      --cc-shared-dir-user     ) shift; local cc_shared_dir_user="$1";     shift;;
+      --cc-shared-dir-password ) shift; local cc_shared_dir_password="$1"; shift;;
 
-      --router-acl-rules       ) shift; router_acl_rules="$1";         shift;;
-      --router-acl-drop-conn   ) shift; router_acl_drop_conn="$1";     shift;;
+      --router-acl-rules       ) shift; local router_acl_rules="$1";       shift;;
+      --router-acl-drop-conn   ) shift; local router_acl_drop_conn="$1";   shift;;
 
       -d | --debug   ) set -x; shift ;;
       -h | --help    ) usage ; exit 1 ;;
@@ -47,6 +89,7 @@ function setup_node() {
     esac
   done
 
+  [ "$#" -eq 0 ]             && usage; exit 1
   [ -z "$core_ip" ]          && message "error" "Missing parameter --core-ip"
   [ -z "$cluster_hostname" ] && message "error" "Missing parameter --cluster-hostname"
   [ -z "$roles" ]            && message "error" "Missing parameter --roles"
@@ -114,11 +157,11 @@ function setup_node() {
 
   roles_setup "$roles" "$core_ip" "$cluster_hostname" \
     "$cc_shared_dir" "$cc_shared_dir_ip" "$cc_shared_dir_user" "$cc_shared_dir_password"
-  
+
   if [ "${roles#core}" == "${roles}" ]; then
     node_attach "$core_ip" "$roles" "$mbus_ip" "$mbus_port"
   fi
-  
+
   roles_post_attach_setup "$roles" "$use_proxy" "$http_proxy" "$http_proxy_port" \
     "$(declare -p ROUTER_PROPERTIES)" "$router_acl_rules" "$router_acl_drop_conn"
 }
@@ -270,7 +313,7 @@ function roles_post_attach_setup() {
   fi
 
   # Configure router
-  if [[ "${roles_array[@]/router}" != "${roles_array[@]}" || 
+  if [[ "${roles_array[@]/router}" != "${roles_array[@]}" ||
         "${roles_array[@]/core}"   != "${roles_array[@]}" ]]; then
     message "info" "> Setup the router"
     router_properties "$(declare -p router_properties)"

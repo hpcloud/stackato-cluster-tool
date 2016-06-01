0e5298882e3116172afd5377d268693b9ecb674c
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-06-01 16:02:18 -0700

    [terraform] Split the proxy and provisioner repo configuration block

--- terraform/openstack/provider-openstack-resources-interface.tf
+++ terraform/openstack/provider-openstack-resources-interface.tf
@@ -1,20 +1,32 @@
 # This file is used to abstract provider specific data in order
 # to use the same references in other Terraform files
 
+# Dynamic variable about the core node
 resource "null_resource" "core" {
   triggers = {
-    private_ip = "${element(concat(openstack_compute_instance_v2.core.*.fixed_ip_v4), 0)}"
+    private_ip = "${openstack_compute_instance_v2.core.access_ip_v4}"
   }
 }
 
+# Dynamic variables about the proxy
 resource "null_resource" "proxy" {
  triggers = {
-   private_ip = "${element(concat(openstack_compute_instance_v2.proxy.*.fixed_ip_v4), 0)}"
-   public_ip  = "${element(concat(openstack_compute_instance_v2.proxy.*.floating_ip), 0)}"
-   apt_proxy  = "${element(concat(openstack_compute_instance_v2.proxy.*.fixed_ip_v4), 0)}"
+   http_proxy      = "${coalesce(lookup(var.proxy, "http_proxy"),      openstack_compute_instance_v2.proxy.network.0.fixed_ip_v4)}"
+   https_proxy     = "${coalesce(lookup(var.proxy, "https_proxy"),     openstack_compute_instance_v2.proxy.network.0.fixed_ip_v4)}"
+   apt_http_proxy  = "${coalesce(lookup(var.proxy, "apt_http_proxy"),  openstack_compute_instance_v2.proxy.network.0.fixed_ip_v4)}"
+   apt_https_proxy = "${coalesce(lookup(var.proxy, "apt_https_proxy"), openstack_compute_instance_v2.proxy.network.0.fixed_ip_v4)}"
  }
 }
 
+# Dynamic variables about the provisioner repository
+resource "null_resource" "provisioner_repo" {
+  triggers = {
+    public_ip  = "${openstack_compute_instance_v2.proxy.access_ip_v4}"
+    private_ip = "${openstack_compute_instance_v2.proxy.network.0.fixed_ip_v4}"
+  }
+}
+
+# Dynamic variables about the network
 resource "null_resource" "network" {
   triggers = {
     cidr_block = "${openstack_networking_subnet_v2.public.cidr}"

18631eb78549db3d285439c2fde11e6709413cd3
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-06-01 16:04:14 -0700

    [terraform] OpenStack - Move all instances to the backend

--- terraform/openstack/provider-openstack-instances.tf
+++ terraform/openstack/provider-openstack-instances.tf
@@ -9,27 +9,13 @@ resource "openstack_compute_instance_v2" "core" {
   image_id = "${lookup(var.openstack_images, var.os_region_name)}"
   flavor_name = "${lookup(var.openstack_flavor_name, "core")}"
   key_pair = "${var.ssh_key_name}"
-  security_groups = ["${openstack_compute_secgroup_v2.public.id}"]
+  security_groups = ["${openstack_compute_secgroup_v2.private.id}"]
   network {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
-  # floating_ip = "${element(openstack_compute_floatingip_v2.floatips.*.address, count.index)}"
-  floating_ip = "${openstack_compute_floatingip_v2.floatips.address}"
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.core.rendered}"
-
-  # Setup the provisioner connection with the Core node
-  connection {
-      user = "stackato"
-      password = "${var.core_password}"
-  }
-
-  # Copies the myapp.conf file to /etc/myapp.conf
-  provisioner "file" {
-    source = "stackato-automation"
-    destination = "/opt/"
-  }
 }
 
 resource "openstack_compute_instance_v2" "dea" {
@@ -47,7 +33,6 @@ resource "openstack_compute_instance_v2" "dea" {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
-  # scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.private.cidr}" }
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.dea.rendered}"
 }
@@ -67,7 +52,6 @@ resource "openstack_compute_instance_v2" "dataservices" {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
-  # scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.private.cidr}" }
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.dataservices.rendered}"
 }
@@ -87,7 +71,6 @@ resource "openstack_compute_instance_v2" "controller" {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
-  # scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.private.cidr}" }
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.controller.rendered}"
 }
@@ -107,8 +90,6 @@ resource "openstack_compute_instance_v2" "router" {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
-  # scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.private.cidr}" }
-  floating_ip = "${element(openstack_compute_floatingip_v2.floatips_routers.*.address, count.index)}"
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.public.cidr}" }
   user_data = "${template_file.router.rendered}"
 }

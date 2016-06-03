22794965adbc64d91b82faf875421e5bd318c317
Stefan Bourlon <stefan.bourlon@hpe.com>
2016-06-03 14:31:27 -0700

    [terraform] OpenStack - Assign a floating IP to the core and router nodes

--- terraform/openstack/provider-openstack-instances.tf
+++ terraform/openstack/provider-openstack-instances.tf
@@ -9,11 +9,12 @@ resource "openstack_compute_instance_v2" "core" {
   image_id = "${lookup(var.openstack_images, var.os_region_name)}"
   flavor_name = "${lookup(var.openstack_flavor_name, "core")}"
   key_pair = "${var.ssh_key_name}"
-  security_groups = ["${openstack_compute_secgroup_v2.private.id}"]
+  security_groups = ["${openstack_compute_secgroup_v2.public.id}"]
   network {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
+  floating_ip = "${openstack_compute_floatingip_v2.floatips.address}"
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.main.cidr}" }
   user_data = "${template_file.core.rendered}"
 }
@@ -90,6 +91,7 @@ resource "openstack_compute_instance_v2" "router" {
     uuid="${openstack_networking_network_v2.main.id}"
     name="${openstack_networking_network_v2.main.name}"
   }
+  floating_ip = "${openstack_compute_floatingip_v2.floatips.address}"
   scheduler_hints { build_near_host_ip="${openstack_networking_subnet_v2.main.cidr}" }
   user_data = "${template_file.router.rendered}"
 }
